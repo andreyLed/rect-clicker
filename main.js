a5206ef65c2086293d3b74124cb90032af0e97e8
sneg <sneg@example.com>
2016-11-18 18:13:45 +0200

    starwars version done

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function game(maxRect) {
-  var rectPool = [];
+function game(maxAlian) {
+  var alianPool = [];
+  var space = new Space();
   var isPlay = false;
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
@@ -15,7 +16,7 @@ function game(maxRect) {
     canvas.addEventListener('click', function (event) {
       var x = event.pageX - elemLeft;
       var y = event.pageY - elemTop;
-      rectPool.forEach(function (element) {
+      alianPool.forEach(function (element) {
         if (x > element.x && x < element.x + element.size && y > element.y && y < element.y + element.size) {
           counter.textContent = ' ' + (Number(counter.textContent) + 1) + ' ';
           element.propsRandomizer();
@@ -28,10 +29,11 @@ function game(maxRect) {
   }
 
   function reset() {
-    rectPool = [];
-    for (var i = 0; i < maxRect; i++) {
-      rectPool[i] = new Rect();
-      rectPool[i].propsRandomizer();
+    
+    alianPool = [];
+    for (var i = 0; i < maxAlian; i++) {
+      alianPool[i] = new Alian();
+      alianPool[i].propsRandomizer();
     }
     counter.textContent = ' 0';
     context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
@@ -52,9 +54,11 @@ function game(maxRect) {
 
   function animate() {
     context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    rectPool.forEach(function (rect) {
-      if (Date.now() > rect.startTime) {
-        rect.draw(context);
+    
+    space.draw(context);
+    alianPool.forEach(function (alian) {
+      if (Date.now() > alian.startTime) {
+        alian.draw(context);
       }
     })
     if (isPlay) {
@@ -65,13 +69,24 @@ function game(maxRect) {
   initGame();
 }
 
-//Класс для создания квадратов
-function Rect() { }
+var images = new function () {
+  this.enemy = new Image();
+  this.space = new Image();
+  this.station = new Image();
+  this.aim = new Image();
 
-//Функция для отрисовки квадрата
-Rect.prototype.draw = function (context) {
-  context.fillStyle = this.color;
-  context.fillRect(this.x, this.y, this.size, this.size);
+  this.enemy.src = './assets/image/enemy.png';
+  this.space.src = './assets/image/bg.png';
+  this.station.src = './assets/image/ship.png';
+  this.aim.src = './assets/image/aim.png';
+}
+
+//Класс для создания инопланетян
+function Alian() { }
+
+//Функция для отрисовки инопланетян
+Alian.prototype.draw = function (context) {
+  context.drawImage(images.enemy, this.x, this.y, this.size, this.size);  
   this.y += this.speed;
   if (this.y >= canvas.clientHeight) {
     this.propsRandomizer();
@@ -79,13 +94,31 @@ Rect.prototype.draw = function (context) {
 }
 
 //Функция для рэндомного изменения свойств квадрата
-Rect.prototype.propsRandomizer = function () {
+Alian.prototype.propsRandomizer = function () {
   this.x = getRandom({ type: 'position', max: (canvas.clientWidth - this.size), min: this.size });
   this.y = 0;
   this.color = getRandom({ type: 'color' });
   this.speed = getRandom({ type: 'speed', max: this.maxSpeed, min: 1 });
   this.startTime = getRandom({ type: 'time', max: this.maxWaitTime });
 }
+
+//Класс для создания космоса(фон)
+function Space() { 
+}
+
+Space.prototype.draw = function (context) {
+  
+  context.drawImage(images.space, 0, 0, canvas.clientWidth, canvas.clientHeight);  
+}
+
+//Класс для создания прицела
+function Aim() { 
+}
+
+Aim.prototype.draw = function (context) {
+  context.drawImage(images.aim, 0, 0, canvas.clientWidth, canvas.clientHeight);  
+}
+
 
 function getRandom(randomObject) {
   switch (randomObject.type) {
@@ -107,11 +140,11 @@ function randomRGBChanelValue() {
 
 document.addEventListener("DOMContentLoaded", function () {
   //Default value
-  var maxRect = 10;
-  Rect.prototype.maxWaitTime = 3000;
-  Rect.prototype.maxSpeed = 2;
-  Rect.prototype.size = 30;
+  var maxAlian = 10;
+  Alian.prototype.maxWaitTime = 3000;
+  Alian.prototype.maxSpeed = 2;
+  Alian.prototype.size = 30;
   
   //game start
-  game(maxRect);
+  game(maxAlian);
 });
